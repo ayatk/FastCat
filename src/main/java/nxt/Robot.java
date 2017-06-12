d11dd844bfd93626b4196208b8522c28ccebd3c3
Aya Tokikaze <git@ayatk.com>
2017-06-12 13:46:49 +0900

    ボタンを押すまで前に進み続けるだけのやつ

--- src/main/java/nxt/Robot.java
+++ src/main/java/nxt/Robot.java
@@ -1,10 +1,23 @@
 package nxt;
 
 import lejos.nxt.Button;
+import lejos.nxt.Motor;
+import lejos.nxt.NXTRegulatedMotor;
 
 public class Robot {
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        NXTRegulatedMotor rightWheel = Motor.A;
+        NXTRegulatedMotor leftWheel = Motor.C;
+
+
+        int speed = 300;
+
+        rightWheel.setSpeed(speed);
+        leftWheel.setSpeed(speed);
+        rightWheel.forward();
+        leftWheel.forward();
+
+        System.out.println("Press any button");
         Button.waitForAnyPress();
     }
 }
