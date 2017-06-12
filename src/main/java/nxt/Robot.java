67c8e067e0b6c60fc97321a5ccc06109cc14edc1
Aya Tokikaze <git@ayatk.com>
2017-06-12 15:44:42 +0900

    色をキャリブレーションして色々見分けるやつ

--- src/main/java/nxt/Robot.java
+++ src/main/java/nxt/Robot.java
@@ -1,13 +1,48 @@
 package nxt;
 
 import lejos.nxt.Button;
-import nxt.sample.MotorControl;
+import lejos.nxt.LCD;
+import lejos.nxt.Sound;
+import lejos.nxt.addon.ColorHTSensor;
+import lejos.robotics.Color;
+import lejos.util.Delay;
+import nxt.sample.ColorCalibration;
 
 public class Robot {
-    public static void main(String[] args) {
-        MotorControl motor = new MotorControl();
 
-        motor.run(400, 500);
+    static String[] colorNames = {
+            "Red", "Green", "Blue", "Yellow", "Magenta", "Orange", "White", "Black",
+            "Pink", "Gray", "Light gray", "Dark gray", "Cyan"};
+
+    public static void main(String[] args) {
+        ColorCalibration colorCalibration = new ColorCalibration();
+
+        ColorHTSensor sensor = colorCalibration.calibration();
+
+        while (!Button.ESCAPE.isDown()) { // ESCAPE押下待ち
+
+            LCD.clear();
+            // カラーIDと名前を表示
+            LCD.drawString("Color ID = ", 0, 1);
+            int id = sensor.getColorID();
+            LCD.drawInt(id, 11, 1);
+            LCD.drawString(colorNames[id], 5, 2);
+
+            // 赤成分を表示
+            Color color = sensor.getColor();
+            LCD.drawString("R", 0, 4);
+            LCD.drawInt(color.getRed(), 1, 5);
+
+            // 緑成分を表示
+            LCD.drawString("G", 4, 4);
+            LCD.drawInt(color.getGreen(), 5, 5);
+            // 青成分を表示
+            LCD.drawString("B", 8, 4);
+            LCD.drawInt(color.getBlue(), 9, 5);
+
+            LCD.refresh();
+            Delay.msDelay(200);
+        }
 
         System.out.println("Press any button");
         Button.waitForAnyPress();
---
+++ src/main/java/nxt/sample/ColorCalibration.java
@@ -0,0 +1,45 @@
+package nxt.sample;
+
+import lejos.nxt.Button;
+import lejos.nxt.LCD;
+import lejos.nxt.SensorPort;
+import lejos.nxt.addon.ColorHTSensor;
+import lejos.util.Delay;
+
+/**
+ * Created by ayatk on 2017/06/12.
+ */
+public class ColorCalibration {
+    public ColorHTSensor calibration() {
+        ColorHTSensor sensor = new ColorHTSensor(SensorPort.S3);
+
+        LCD.drawString("Starting calibration...", 0, 1);
+        Delay.msDelay(1000);
+
+        LCD.drawString("Black Level", 0, 1);
+
+        Button.ENTER.waitForPress();
+
+        if (sensor.initBlackLevel() == 0) {
+            LCD.drawString("OK", 0, 1);
+        }else {
+            LCD.drawString("No Good...", 0, 1);
+        }
+
+        Button.ENTER.waitForPress();
+
+        LCD.drawString("White Level", 0, 1);
+
+        Button.ENTER.waitForPress();
+
+        if (sensor.initWhiteBalance() == 0) {
+            LCD.drawString("OK", 0, 1);
+        }else {
+            LCD.drawString("No Good...", 0, 1);
+        }
+
+        Button.ENTER.waitForPress();
+
+        return sensor;
+    }
+}
